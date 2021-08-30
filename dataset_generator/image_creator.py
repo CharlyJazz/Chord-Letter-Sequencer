5fa1a3f6095793a2953b664183e5fcdc5728568a
Carlos Azuaje <carlos.azuaje@moove-it.com>
2021-08-29 23:52:25 -0300

    add more methods to the pipeline

--- dataset_generator/image_creator.py
+++ dataset_generator/image_creator.py
@@ -15,15 +15,15 @@ class ImageCreator():
     def __init__(self, chord_instance: Chord):
         self.chord = chord_instance
         self.fonts_path = [
-            "/Users/charlyjazz/Charlyjazz/Chord-Letter-Sequencer/dataset_generator/fonts/Cooljazz.ttf",
-            # "/Users/charlyjazz/Charlyjazz/Chord-Letter-Sequencer/dataset_generator/fonts/arial.ttf"
+            # "/Users/charlyjazz/Charlyjazz/Chord-Letter-Sequencer/dataset_generator/fonts/Cooljazz.ttf",
+            "/Users/charlyjazz/Charlyjazz/Chord-Letter-Sequencer/dataset_generator/fonts/arial.ttf"
         ]
         # Each key is the label value of the symbol and
         # we going to use severals symbol versions for the
         # same label value
         self.symbols = {
-            "major": ["", "ma", "M", "Maj", ], #"/Users/charlyjazz/Charlyjazz/Chord-Letter-Sequencer/dataset_generator/symbols/major_triangle.png"
-            "minor": ["-", "m", "min", "mi"],
+            "major": ["", "ma", "M", "Maj"], #"/Users/charlyjazz/Charlyjazz/Chord-Letter-Sequencer/dataset_generator/symbols/major_triangle.png"
+            "minor": ["-", "m", "minor"],
             "sharp": ["#"],
             "bimol": ["b"],
             "half disminished": ['Ø'],
@@ -34,7 +34,6 @@ class ImageCreator():
 
         # Creating Image State
 
-        self.chord_string_length = None
         self.font_size = None
         self.font_family = None
         self.truetype = None
@@ -42,20 +41,12 @@ class ImageCreator():
         self.draw = None
         self.x_offset = 0.0
 
-    # This method need measure the real chord length because
-    # word like disminished will be replace with a symbol then len(str)
-    # is wrong because if the image wil be a symbol with the length
-    # of a single character
-    def len_chord(self, string):
-        return len(string)
-
     # Create image of the chord to save
     def create_image(self):
-        self.chord_string_length = self.len_chord(str(self.chord))
         self.font_size = choice(self.font_sizes)
         self.font_family = choice(self.fonts_path)
         self.truetype = ImageFont.truetype(self.font_family, self.font_size)
-        self.image = Image.new('RGB', (int((self.font_size / 1.5) * self.chord_string_length), 100), color=(255, 255, 255))
+        self.image = Image.new('RGB', (240, 60), color=(255, 255, 255))
         self.draw = ImageDraw.Draw(self.image)
 
         # Array of functions to split concerns and condionality
@@ -68,17 +59,22 @@ class ImageCreator():
         # Show image
         self.open_cv_show_image()
 
+    # Helper for print and debug
     def debug(self, label, value):
         print(f' - - [DEBUG] - {label} - {value}')
 
     # Useful pipeline of functions to send offset x in each function and
-    # Separate concerns and and complex logic in each function
+    # Separate concerns and complex logic in each function
     def pipeline_of_texts(self):
         return [
             self.draw_pitch,
             self.draw_accidentals,
+            self.draw_quality,
             self.draw_bass_slash_note,
-            self.draw_quality
+            self.draw_th_type,
+            self.draw_interval_5th_accidental,
+            self.draw_add_notes,
+            self.draw_sus_type
         ]
 
     # Draw Pitch in the image
@@ -102,6 +98,9 @@ class ImageCreator():
     # Draw Quality with letters or symbols (random choice)
     # Quality word are minor, major, augmented, disminished and half disminished
     def draw_quality(self):
+        # Dont draw quality if the chord is a sus4 or sus2
+        if self.chord.sus_type:
+            return
         # Chose a symbol or word to replace the quality word
         synonymous = choice(self.symbols[self.chord.quality])
         fontsizes = range(13, 18)
@@ -121,6 +120,35 @@ class ImageCreator():
         else:
             pass
 
+    # Draw the th interval type (7, 9, 11, 13, 15)
+    def draw_th_type(self):
+        if self.chord.th_type:
+            self.draw.text((self.x_offset, self.font_size / 2), self.chord.th_type, font=self.truetype, fill=(0, 0, 0))
+            self.x_offset = self.draw.textlength(self.chord.th_type, self.truetype) + self.x_offset
+
+    # Draw if the 5th is bemol or sharp
+    def draw_interval_5th_accidental(self):
+        if self.chord.interval_5th_accidental:
+            string = f"{self.chord.interval_5th_accidental}5"
+            self.draw.text((self.x_offset, self.font_size / 2), string, font=self.truetype, fill=(0, 0, 0))
+            self.x_offset = self.draw.textlength(string, self.truetype) + self.x_offset
+
+    # Draw the added notes
+    def draw_add_notes(self):
+        if len(self.chord.extra_notes):
+            for i in self.chord.extra_notes:
+                string = f"(add{i})"
+                self.draw.text((self.x_offset, self.font_size / 2), string, font=self.truetype, fill=(0, 0, 0))
+                self.x_offset = self.draw.textlength(string, self.truetype) + self.x_offset
+
+    # Draw the added notes
+    def draw_sus_type(self):
+        if self.chord.sus_type:
+            string = self.chord.sus_type
+            self.draw.text((self.x_offset, self.font_size / 2), string, font=self.truetype, fill=(0, 0, 0))
+            self.x_offset = self.draw.textlength(string, self.truetype) + self.x_offset
+
+    # Helper to render image
     def open_cv_show_image(self):
         self.debug('Chord', self.chord)
         self.image.show()
--- dataset_generator/run.py
+++ dataset_generator/run.py
@@ -10,7 +10,11 @@ Cminor = Chord("C", "", "minor")
 Chalfdimismished = Chord("C", "", "half disminished")
 Cdisminished = Chord("C", "#", "disminished")
 Caugmented = Chord("C", "#", "augmented")
+CSharpMinor7 = Chord("C", "#", "minor", interval_5th_accidental="b", th_type="7", extra_notes=['6'])
+# CSharpSus4 = Chord("A", "#", "minor", bass_slash_note="B#")
+
 
 if __name__ == '__main__':
-    image_creator = ImageCreator(ChordConstructorRandom().chord_creation_flow())
+    # image_creator = ImageCreator(ChordConstructorRandom().chord_creation_flow())
+    image_creator = ImageCreator(CSharpMinor7)
     image_creator.create_image()
