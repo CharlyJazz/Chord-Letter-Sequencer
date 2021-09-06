1e85438dcb4c5f5694fd0d8dfc9114a466e00794
Carlos Azuaje <carlos.azuaje@moove-it.com>
2021-09-05 21:00:41 -0300

    add more variability to the datasets

--- dataset_generator/image_creator.py
+++ dataset_generator/image_creator.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 from chord import Chord
+import random
 
 # import cv2
 
@@ -11,11 +12,17 @@ from numpy import array
 # Diminished	Cdim Co	C, Eb, Gb
 # Augmented	Caug C+ C+5	C, E, G#
 
+# https://www.myfonts.com/fonts/tijs-krammer/chord-symbols/
+
+def random_boolean():
+    return bool(random.getrandbits(1))
+
 class ImageCreator():
-    def __init__(self, chord_instance: Chord):
+    def __init__(self, chord_instance: Chord, debug_mode=True):
+        self.debug_mode = debug_mode
         self.chord = chord_instance
         self.fonts_path = [
-            # "/Users/charlyjazz/Charlyjazz/Chord-Letter-Sequencer/dataset_generator/fonts/Cooljazz.ttf",
+            "/Users/charlyjazz/Charlyjazz/Chord-Letter-Sequencer/dataset_generator/fonts/Cooljazz.ttf",
             "/Users/charlyjazz/Charlyjazz/Chord-Letter-Sequencer/dataset_generator/fonts/arial.ttf"
         ]
         # Each key is the label value of the symbol and
@@ -30,23 +37,30 @@ class ImageCreator():
             "disminished": ["○", "dim"],
             "augmented": ['+', 'aug']
         }
-        self.font_sizes = [35]
+        self.font_sizes = range(25, 35)
 
         # Creating Image State
 
         self.font_size = None
+        self.font_size_for_symbols_to_top_and_small = None
+        self.font_size_for_dash_minor_symbol = None # '-'
         self.font_family = None
         self.truetype = None
         self.image = None
         self.draw = None
         self.x_offset = 0.0
+        self.symbols_to_top_and_small = random_boolean()
 
     # Create image of the chord to save
     def create_image(self):
         self.font_size = choice(self.font_sizes)
+        # Ok You going to be big as self.font_size because the "-" symbol is tiny
+        self.font_size_for_dash_minor_symbol = self.font_size
+        # Ok You going to be more smaller than the general font_size
+        self.font_size_for_symbols_to_top_and_small = choice(range(self.font_size - 15, self.font_size - 5))
         self.font_family = choice(self.fonts_path)
         self.truetype = ImageFont.truetype(self.font_family, self.font_size)
-        self.image = Image.new('RGB', (240, 60), color=(255, 255, 255))
+        self.image = Image.new('RGB', (280, 60), color=(255, 255, 255))
         self.draw = ImageDraw.Draw(self.image)
 
         # Array of functions to split concerns and condionality
@@ -57,11 +71,13 @@ class ImageCreator():
             fn()
 
         # Show image
-        self.open_cv_show_image()
+        if self.debug_mode:
+            self.open_cv_show_image()
 
     # Helper for print and debug
     def debug(self, label, value):
-        print(f' - - [DEBUG] - {label} - {value}')
+        if self.debug_mode:
+            print(f' - - [DEBUG] - {label} - {value}')
 
     # Useful pipeline of functions to send offset x in each function and
     # Separate concerns and complex logic in each function
@@ -101,20 +117,30 @@ class ImageCreator():
         # Dont draw quality if the chord is a sus4 or sus2
         if self.chord.sus_type:
             return
+
         # Chose a symbol or word to replace the quality word
         synonymous = choice(self.symbols[self.chord.quality])
-        fontsizes = range(13, 18)
+        fontsizes = range(20, 30)
         fontsize = int(choice(fontsizes))
+        
         if ".png" in synonymous:
             image_symbol = Image.open(synonymous)
             image_symbol.thumbnail((fontsize, fontsize))
             image_symbol = image_symbol.convert("RGB").copy()
             self.image.paste(image_symbol, (int(self.x_offset),  int(self.font_size / 2)))
-            # Revisar esto
             self.x_offset = self.x_offset + fontsize
         elif len(synonymous):
-            font = ImageFont.truetype(self.font_family, fontsize)
-            y = self.font_size - fontsize # Responsivity
+            # Case special to center the "-" symbol
+            if synonymous == "-": 
+                fontsize = self.font_size_for_dash_minor_symbol
+                font = ImageFont.truetype(self.font_family, fontsize)
+                y = int(self.font_size / 2) + 2
+            else:
+                # Case special when you want the symbols more to the top and smaller
+                if self.symbols_to_top_and_small:
+                    fontsize = self.font_size_for_symbols_to_top_and_small
+                font = ImageFont.truetype(self.font_family, fontsize)
+                y = self.font_size - fontsize
             self.draw.text((self.x_offset, y), synonymous, font=font, fill=(0, 0, 0))
             self.x_offset = self.draw.textlength(synonymous, font) + self.x_offset
         else:
@@ -123,23 +149,48 @@ class ImageCreator():
     # Draw the th interval type (7, 9, 11, 13, 15)
     def draw_th_type(self):
         if self.chord.th_type:
-            self.draw.text((self.x_offset, self.font_size / 2), self.chord.th_type, font=self.truetype, fill=(0, 0, 0))
-            self.x_offset = self.draw.textlength(self.chord.th_type, self.truetype) + self.x_offset
+            # Case special when you want the symbols more to the top and smaller
+            if self.symbols_to_top_and_small:
+                fontsize = self.font_size_for_symbols_to_top_and_small
+                font = ImageFont.truetype(self.font_family, fontsize)
+                y = self.font_size - fontsize
+                self.draw.text((self.x_offset, y), self.chord.th_type, font=font, fill=(0, 0, 0))
+                self.x_offset = self.draw.textlength(self.chord.th_type, font) + self.x_offset
+            else:
+                self.draw.text((self.x_offset, self.font_size / 2), self.chord.th_type, font=self.truetype, fill=(0, 0, 0))
+                self.x_offset = self.draw.textlength(self.chord.th_type, self.truetype) + self.x_offset
 
     # Draw if the 5th is bemol or sharp
     def draw_interval_5th_accidental(self):
         if self.chord.interval_5th_accidental:
             string = f"{self.chord.interval_5th_accidental}5"
-            self.draw.text((self.x_offset, self.font_size / 2), string, font=self.truetype, fill=(0, 0, 0))
-            self.x_offset = self.draw.textlength(string, self.truetype) + self.x_offset
+            # Case special when you want the symbols more to the top and smaller
+            if self.symbols_to_top_and_small:
+                fontsize = self.font_size_for_symbols_to_top_and_small
+                font = ImageFont.truetype(self.font_family, fontsize)
+                y = self.font_size - fontsize
+                self.draw.text((self.x_offset, y), string, font=font, fill=(0, 0, 0))
+                self.x_offset = self.draw.textlength(string, font) + self.x_offset
+            else:
+                self.draw.text((self.x_offset, self.font_size / 2), string, font=self.truetype, fill=(0, 0, 0))
+                self.x_offset = self.draw.textlength(string, self.truetype) + self.x_offset
 
     # Draw the added notes
     def draw_add_notes(self):
+        with_parenthesis = random_boolean()
         if len(self.chord.extra_notes):
             for i in self.chord.extra_notes:
-                string = f"(add{i})"
-                self.draw.text((self.x_offset, self.font_size / 2), string, font=self.truetype, fill=(0, 0, 0))
-                self.x_offset = self.draw.textlength(string, self.truetype) + self.x_offset
+                string = f"(add{i})" if with_parenthesis else f" add{i}"
+                # Case special when you want the symbols more to the top and smaller
+                if self.symbols_to_top_and_small:
+                    fontsize = self.font_size_for_symbols_to_top_and_small
+                    font = ImageFont.truetype(self.font_family, fontsize)
+                    y = self.font_size - fontsize
+                    self.draw.text((self.x_offset, y), string, font=font, fill=(0, 0, 0))
+                    self.x_offset = self.draw.textlength(string, font) + self.x_offset
+                else:
+                    self.draw.text((self.x_offset, self.font_size / 2), string, font=self.truetype, fill=(0, 0, 0))
+                    self.x_offset = self.draw.textlength(string, self.truetype) + self.x_offset
 
     # Draw the added notes
     def draw_sus_type(self):
@@ -150,10 +201,11 @@ class ImageCreator():
 
     # Helper to render image
     def open_cv_show_image(self):
-        self.debug('Chord', self.chord)
-        self.image.show()
-        # cv_img = array(self.image)
-        # cv_img = cv_img[:, :, ::-1].copy()
-        # cv2.imshow(str(self.chord), cv_img)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        if self.debug_mode:
+            self.debug('Chord', self.chord)
+            self.image.show()
+            # cv_img = array(self.image)
+            # cv_img = cv_img[:, :, ::-1].copy()
+            # cv2.imshow(str(self.chord), cv_img)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
--- dataset_generator/run.py
+++ dataset_generator/run.py
@@ -2,19 +2,39 @@ from chord_constructor_random import ChordConstructorRandom
 from image_creator import ImageCreator
 from chord import Chord
 
+import argparse
+
+def non_or_str(value):
+    if value == None:
+        return None
+    return value
+
 # https://www.pianochord.org/chord-symbols.html
 
-# Cases: Triangle Symbol, M letter, Maj word
-Cmajor = Chord("C", "#", "major", bass_slash_note="A")
-Cminor = Chord("C", "", "minor")
-Chalfdimismished = Chord("C", "", "half disminished")
-Cdisminished = Chord("C", "#", "disminished")
-Caugmented = Chord("C", "#", "augmented")
-CSharpMinor7 = Chord("C", "#", "minor", interval_5th_accidental="b", th_type="7", extra_notes=['6'])
+# Cmajor = Chord("C", "#", "major", bass_slash_note="A")
+# Cminor = Chord("C", "", "minor")
+# Chalfdimismished = Chord("C", "", "half disminished")
+# Cdisminished = Chord("C", "#", "disminished")
+# Caugmented = Chord("C", "#", "augmented")
+# Chord = Chord("C", "#", "minor", interval_5th_accidental="b", th_type="7", extra_notes=['9'], bass_slash_note="F")
 # CSharpSus4 = Chord("A", "#", "minor", bass_slash_note="B#")
 
-
 if __name__ == '__main__':
-    # image_creator = ImageCreator(ChordConstructorRandom().chord_creation_flow())
-    image_creator = ImageCreator(CSharpMinor7)
-    image_creator.create_image()
+    parser = argparse.ArgumentParser(description='OCR using CNN+RNN+CTC')
+    parser.add_argument('--path', type=non_or_str, help='path to the directory')
+    parser.add_argument('--count', type=int, help="No. of images to create")
+    args = parser.parse_args() 
+
+    if args.count and args.path:
+        for i in range(args.count):
+            chord_instance = ChordConstructorRandom().chord_creation_flow()
+            image_creator = ImageCreator(chord_instance, False)
+            image_creator.create_image()
+            # Lol
+            label = str(image_creator.chord).replace("/", " slash ")
+            image_creator.image.save(f"/{args.path}/{label}.png")
+    else:
+        image_creator = ImageCreator(ChordConstructorRandom().chord_creation_flow())
+        image_creator.create_image()
+
+# python3 dataset_generator/run.py --path /Users/charlyjazz/Charlyjazz/Chord-Letter-Sequencer/dataset --count 2
